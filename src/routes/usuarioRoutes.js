9c8b6a554702a7523a17f0fdb74197509ee4f355
danielpalli <dpallicolque@est.frba.utn.edu.ar>
2022-07-01 13:45:45 -0300

    Agregadas rutas para confirmar token y login

--- src/routes/usuarioRoutes.js
+++ src/routes/usuarioRoutes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import {
-    crearUsuario,
+    confirmarTokenUsuario,
+    crearUsuario, loginUsuario,
     obtenerUsuarios,
 } from '../controllers/usuarioController.js';
 
@@ -8,9 +9,7 @@ const router = express.Router();
 
 router.get('/', obtenerUsuarios);
 router.post('/crear', crearUsuario);
-router.post('/login', (req, res)=>{
-    const {body} = req;
-    console.log(body);
-});
+router.post('/login', loginUsuario);
+router.get('/confirmar/:token', confirmarTokenUsuario);
 
 export default router;
