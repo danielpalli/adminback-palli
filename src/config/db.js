7b4bdd48b12d05b973f5267264a8e0fe71aa3da7
danielpalli <dpallicolque@est.frba.utn.edu.ar>
2022-07-03 16:33:26 -0300

    Prettier format

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,14 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 export const conectarDB = async () => {
-    try {
-        const connection = await mongoose.connect('mongodb+srv://dbUser:fpYzfHt5ipmeD91D@micluster.dkelb.mongodb.net/?retryWrites=true&w=majority');
-        console.log(connection.connection.host);
-        console.log(connection.connection.port);
-    } catch (err) {
-        console.log(err);
-        process.exit(1);
-
-    }
+  try {
+    const connection = await mongoose.connect(
+      'mongodb+srv://dbUser:fpYzfHt5ipmeD91D@micluster.dkelb.mongodb.net/?retryWrites=true&w=majority'
+    );
+    console.log(connection.connection.host);
+    console.log(connection.connection.port);
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
 };
