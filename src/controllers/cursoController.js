b8437b66ff8a2cc68f6cf1fb27f3816b5fc8a2fa
Daniel Palli <feerhh91@gmail.com>
2022-07-23 04:17:13 -0300

    Update curso controller

--- src/controllers/cursoController.js
+++ src/controllers/cursoController.js
@@ -23,7 +23,7 @@ export const nuevoCurso = async (req, res) => {
 
     const curso = new Curso(req.body);
 
-    // await curso.save();
+    await curso.save();
     res.json({
       ok: true,
       curso,
@@ -38,12 +38,113 @@ export const nuevoCurso = async (req, res) => {
 };
 
 export const obtenerCurso = async (req, res) => {
-    
+  const { id } = req.params;
+
+  const curso = await Curso.findById(id);
+
+  if (!curso) {
+    const error = new Error('Curso no encontrado');
+    return res.status(404).json({
+      ok: false,
+      msg: error.message,
+    });
+  }
+
+  if (curso.docente.toString() !== req.usuario._id.toString()) {
+    const error = new Error('No tienes permisos para ver este curso');
+    return res.status(401).json({
+      ok: false,
+      msg: error.message,
+    });
+  }
+
+  res.json({
+    ok: true,
+    curso,
+  });
 };
 
-export const editarCurso = async (req, res) => {};
+export const editarCurso = async (req, res) => {
+  const { id } = req.params;
 
-export const eliminarCurso = async (req, res) => {};
+  const curso = await Curso.findById(id);
+
+  if (!curso) {
+    const error = new Error('Curso no encontrado');
+    return res.status(404).json({
+      ok: false,
+      msg: error.message,
+    });
+  }
+
+  if (curso.docente.toString() !== req.usuario._id.toString()) {
+    const error = new Error('No tienes permisos para ver este curso');
+    return res.status(401).json({
+      ok: false,
+      msg: error.message,
+    });
+  }
+
+  curso.nombre = req.body.nombre || curso.nombre;
+  curso.comision = req.body.comision || curso.comision;
+  curso.descripcion = req.body.descripcion || curso.descripcion;
+  curso.horas = req.body.horas || curso.horas;
+  curso.docente = req.body.docente || curso.docente;
+  curso.fechaInicio = req.body.fechaInicio || curso.fechaInicio;
+  curso.fechaFin = req.body.fechaFin || curso.fechaFin;
+  curso.estado = req.body.estado || curso.estado;
+  curso.cupo = req.body.cupo || curso.cupo;
+
+  try {
+    const cursoAlmacenado = await curso.save();
+
+    res.json({
+      ok: true,
+      cursoAlmacenado,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      ok: false,
+      msg: 'Hable con el administrador',
+    });
+  }
+};
+
+export const eliminarCurso = async (req, res) => {
+  const { id } = req.params;
+
+  const curso = await Curso.findById(id);
+
+  if (!curso) {
+    const error = new Error('Curso no encontrado');
+    return res.status(404).json({
+      ok: false,
+      msg: error.message,
+    });
+  }
+
+  if (curso.docente.toString() !== req.usuario._id.toString()) {
+    const error = new Error('No tienes permisos para ver este curso');
+    return res.status(401).json({
+      ok: false,
+      msg: error.message,
+    });
+  }
+  try {
+    await curso.deleteOne();
+    res.json({
+      ok: true,
+      msg: 'Curso eliminado',
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      ok: false,
+      msg: 'Hable con el administrador',
+    });
+  }
+};
 
 export const agregarAlumno = async (req, res) => {};
 
